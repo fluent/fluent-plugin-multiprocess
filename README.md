ca1a66061957ef74573dc746aac819d76e6f6c9e
FURUHASHI Sadayuki <frsyuki@users.sourceforge.jp>
2013-09-17 14:41:11 -0700

    first import

---
+++ README.md
@@ -0,0 +1,35 @@
+# Multiprocess agent plugin for Fluentd
+
+## multiprocess
+
+**multiprocess** agent plugin runs some child fluentd processes.
+
+
+### Configuration
+
+    <source>
+      type multiprocess
+
+      # optional:
+      #graceful_kill_interval 2s
+      #graceful_kill_interval_increment 3s
+      #graceful_kill_timeout 60s
+
+      <process>
+        cmdline -c /etc/fluent/fluentd_child1.conf
+      </process>
+      <process>
+        cmdline -c /etc/fluent/fluentd_child2.conf
+        sleep_before_start 5s
+      </process>
+      <process>
+        cmdline -c /etc/fluent/fluentd_child3.conf
+        sleep_before_shutdown 5s
+      </process>
+    </source>
+
+- **process** section sets command line arguments of a child process. This plugin creates one child process for each \<process\> section
+- **cmdline** option is required in a \<process\> section
+- **sleep\_before\_start** sets wait time before starting the process. Note that child processes **start from last to first** (`fluentd_child3` -\> `sleep 5` -\> { `fluentd_child2`, `fluentd_child1` } in this case)
+- **sleep\_before\_shutdown** sets wait time before shutting down the process. Note that child processes **shutdown from first to last** ({ `fluentd_child1`, `fluentd_child2` } -\> `sleep 5` -> `fluentd_child3` in this case)
+
---
+++ lib/fluent/plugin/in_multiprocess.rb
@@ -0,0 +1,84 @@
+#
+# Fluent
+#
+# Copyright (C) 2013 FURUHASHI Sadayuki
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+module Fluent
+
+  class MultiprocessInput < Input
+    Plugin.register_input('multiprocess', self)
+
+    require 'shellwords'
+    require 'serverengine'
+
+    config_param :graceful_kill_interval, :time, :default => 2
+    config_param :graceful_kill_interval_increment, :time, :default => 3
+    config_param :graceful_kill_timeout, :time, :default => 60
+
+    class ProcessElement
+      include Configurable
+
+      config_param :cmdline, :string
+      config_param :sleep_before_start, :time, :default => 0
+      config_param :sleep_before_shutdown, :time, :default => 0
+
+      attr_accessor :process_monitor
+    end
+
+    def configure(conf)
+      @processes = conf.elements.select {|e|
+        e.name == 'process'
+      }.map {|e|
+        pe = ProcessElement.new
+        pe.configure(e)
+        pe
+      }
+    end
+
+    def start
+      @pm = ServerEngine::ProcessManager.new(
+        :auto_tick => true,
+        :auto_tick_interval => 1,
+        :graceful_kill_interval => @graceful_kill_interval,
+        :graceful_kill_interval_increment => @graceful_kill_interval_increment,
+        :graceful_kill_timeout => @graceful_kill_timeout,
+        :graceful_kill_signal => 'TERM',
+        :immediate_kill_timeout => 0,  # disabled
+      )
+
+      plugin_rb = $LOADED_FEATURES.find {|x| x =~ /fluent\/plugin\.rb\z/ }
+      fluentd_rb = File.join(File.dirname(plugin_rb), 'command', 'fluentd.rb')
+
+      @processes.reverse_each do |pe|
+        cmd = "#{Shellwords.shellescape(RbConfig.ruby)} #{Shellwords.shellescape(fluentd_rb)} #{pe.cmdline}"
+        sleep pe.sleep_before_start if pe.sleep_before_start > 0
+        $log.info "launching child fluentd #{pe.cmdline}"
+        pe.process_monitor = @pm.spawn(cmd)
+      end
+    end
+
+    def shutdown
+      @processes.each {|pe|
+        sleep pe.sleep_before_shutdown if pe.sleep_before_shutdown > 0
+        $log.info "shutting down child fluentd #{pe.cmdline}"
+        pe.process_monitor.start_graceful_stop!
+      }
+      @processes.each {|pe|
+        pe.process_monitor.join
+      }
+    end
+  end
+
+end
